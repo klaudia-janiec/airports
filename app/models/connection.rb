67ef9386a0f122488ac47d65df6a07e4fd7f25ed
klaudia <klaudia.janiec@gmail.com>
2018-01-02 19:44:33 +0100

    Validate source airport and destination airport difference

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -3,6 +3,13 @@ class Connection < ApplicationRecord
   belongs_to :airport_b, class_name: "Airport"
 
   validates :airport_a, uniqueness: { scope: :airport_b }
+  validate  :different_airports
 
   default_scope { includes(:airport_a, :airport_b) }
+
+  private
+
+  def different_airports
+    errors.add(:base, :different_airports) if airport_a&.id == airport_b&.id
+  end
 end
