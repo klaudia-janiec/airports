cb29339a99568a70f1297103b0269d78ec31711a
klaudia <klaudia.janiec@gmail.com>
2018-01-12 16:03:31 +0100

    Add more tests

--- spec/features/create_new_connection_spec.rb
+++ spec/features/create_new_connection_spec.rb
@@ -4,9 +4,9 @@ feature "Create new connection", js: true do
   given!(:krakow_airport) { Fabricate(:airport) }
   given!(:buenos_aires_airport) { Fabricate(:airport, city: "Buenos Aires", country: "Argentina", y: -34.8222, x: -58.5358, icao: "AIRS") }
 
-  scenario "creates new connection" do
-    visit '/connections/new'
+  background { visit '/connections/new' }
 
+  scenario "creates new connection" do
     within('#new_connection') do
       select "Argentina", from: "country_a_name"
       expect(find('#connection_airport_a_id').disabled?).to eq(false)
@@ -19,5 +19,39 @@ feature "Create new connection", js: true do
 
     click_button 'Submit'
     expect(current_path).to eq("/connections")
+  end
+
+  context "when source airport is the same as destination airport" do
+    scenario "doesn't create new connection" do
+      within('#new_connection') do
+        select "Argentina", from: "country_a_name"
+        select "AIRS, Fake airport", from: "connection_airport_a_id"
+
+        select "Argentina", from: "country_b_name"
+        select "AIRS, Fake airport", from: "connection_airport_b_id"
+      end
+
+      click_button 'Submit'
+      expect(current_path).to eq("/connections")
+      expect(page).to have_text("Source airport and destination airport should be different")
+    end
+  end
+
+  context "when connection already exists" do
+    background { Connection.create!(airport_a: buenos_aires_airport, airport_b: krakow_airport) }
+
+    scenario "doesn't create new connection" do
+      within('#new_connection') do
+        select "Argentina", from: "country_a_name"
+        select "AIRS, Fake airport", from: "connection_airport_a_id"
+
+        select "Poland", from: "country_b_name"
+        select "KKOO, Fake airport", from: "connection_airport_b_id"
+      end
+
+      click_button 'Submit'
+      expect(current_path).to eq("/connections")
+      expect(page).to have_text("Source port has already been taken")
+    end
   end
 end
